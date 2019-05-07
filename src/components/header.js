42707a8f11dbb68bf83aef9f0ce6efcb48fc341e
Michael Adesina <michael.adesina@bigpublishing.com>
2019-05-07 14:51:52 +0100

    final

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,20 @@
-import React, { Component } from 'react';
-import '../css/styles.module.css'
+import React  from 'react';
+import classes from '../css/styles.module.css'
 
-class Header extends Component {
-
-    state = { //React wll re_render application when state changes
-        active: 'active',
-        keywords: ''
-    }
-    inputChangeHandler = (event) => {
-        const value = event.target.value === '' ? 'active' : 'non-active';
-        this.setState({
-            active: value,
-            keywords: event.target.value
-        })
-
-    }
-    render() {
-
-
-        console.log(this.state.keywords)
+const  Header = (props) => {
+   
         return (
 
-            <header className={this.state.active}>
+            <header className={classes.logo}>
                 <div className="logo">Logo</div>
 
                 <input
-                    type="text" onChange={this.inputChangeHandler} />
+                    type="text" onChange={props.keywords} />
 
             </header>
 
 
         )
-
-    }
 
 }
 
--- src/index.js
+++ src/index.js
@@ -13,15 +13,32 @@ import NewsList from './components/news_list'
 class App extends Component {
 
     state = {
-        news: JSON
+        news: JSON,
+        filtered: []
+    }
+
+    getKeyword = (event) => {
+        //console.log(event.target.value)
+        let keyword = event.target.value;
+        let filtered = this.state.news.filter((item) => {
+            return item.title.indexOf(keyword) > -1
+        })
+        this.setState({
+            filtered
+        })
+        console.log(filtered)
     }
 
     render() {
+
+        let newsWhole= this.state.news;
+        let newsFiltered = this.state.filtered;
         console.log(this.state.news)
         return (
             <div>
-                <Header />
-                <NewsList news={this.state.news} />
+                <Header keywords={this.getKeyword}/>
+                <NewsList news={newsFiltered.length === 0 
+                    ? newsWhole : newsFiltered} />
 
 
             </div>
